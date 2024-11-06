afb81662f1812df55c234b41d32b0addec63a5aa
Roxana Flores <rextro@MacBook-Pro-de-Roxana.local>
2024-11-06 10:06:47 -0600

    division fixed

--- maquinaTuringDivision.py
+++ maquinaTuringDivision.py
@@ -5,18 +5,24 @@ class TuringMachineRomanDivision:
         self.states = {"INITIAL", "DIVIDE", "SIMPLIFY", "HALT"}
         self.current_state = "INITIAL"
     
-    # Función para convertir un símbolo romano en un conjunto de "I" en la cinta
-    def expand_roman(self, symbol):
+    # Función para convertir un número romano en un conjunto de "I" en la cinta
+    def expand_roman(self, roman):
+        # Valores de cada símbolo romano
         mapping = {
-            "I": "I", 
-            "V": "I" * 5, 
-            "X": "I" * 10, 
-            "L": "I" * 50,  
-            "C": "I" * 100,  
-            "D": "I" * 500,  
-            "M": "I" * 1000  
+            "I": 1, "V": 5, "X": 10, "L": 50, 
+            "C": 100, "D": 500, "M": 1000
         }
-        return list(mapping.get(symbol, ""))  # Devuelve la lista de "I"
+        result = 0
+        length = len(roman)
+        
+        # Convertir el número romano a su equivalente en "I" (valor entero)
+        for i in range(length):
+            if i + 1 < length and mapping[roman[i]] < mapping[roman[i + 1]]:
+                result -= mapping[roman[i]]
+            else:
+                result += mapping[roman[i]]
+        
+        return ["I"] * result  # Expande el valor entero en "I"
 
     # Función principal que ejecuta la máquina de Turing
     def run(self):
@@ -33,19 +39,9 @@ class TuringMachineRomanDivision:
     
     # Estado Inicial: Convierte ambos números romanos en conjuntos de "I"
     def state_initial(self):
-        dividend = []
-        divisor = []
-        before_separator = True  # Marca si estamos leyendo el dividendo o el divisor
-
-        # Convertimos los símbolos a "I" y separamos en dividendo y divisor
-        for symbol in self.tape:
-            if symbol == "#":
-                before_separator = False
-                continue
-            if before_separator:
-                dividend.extend(self.expand_roman(symbol))
-            else:
-                divisor.extend(self.expand_roman(symbol))
+        parts = ''.join(self.tape).split("#")
+        dividend = self.expand_roman(parts[0])
+        divisor = self.expand_roman(parts[1])
 
         self.tape = dividend + ["#"] + divisor
         self.current_state = "DIVIDE"
@@ -69,10 +65,9 @@ class TuringMachineRomanDivision:
         
         # Actualizamos la cinta con el cociente en forma de "I"
         self.tape = ["I"] * quotient_count
-        print("Cociente en 'I' antes de simplificar:", ''.join(self.tape))  # Depuración
         self.current_state = "SIMPLIFY"
     
-    # Estado de Simplificación: Convierte "I" en V, V en X, etc., para notación romana correcta
+    # Estado de Simplificación: Convierte "I" en notación romana correcta
     def state_simplify(self):
         i_count = self.tape.count("I")
         self.tape = []  # Reiniciamos la cinta
@@ -81,48 +76,47 @@ class TuringMachineRomanDivision:
         while i_count >= 1000:
             self.tape.append("M")
             i_count -= 1000
-        while i_count >= 900:  # Manejo especial de CM
+        while i_count >= 900:
             self.tape.append("CM")
             i_count -= 900
         while i_count >= 500:
             self.tape.append("D")
             i_count -= 500
-        while i_count >= 400:  # Manejo especial de CD
+        while i_count >= 400:
             self.tape.append("CD")
             i_count -= 400
         while i_count >= 100:
             self.tape.append("C")
             i_count -= 100
-        while i_count >= 90:  # Manejo especial de XC
+        while i_count >= 90:
             self.tape.append("XC")
             i_count -= 90
         while i_count >= 50:
             self.tape.append("L")
             i_count -= 50
-        while i_count >= 40:  # Manejo especial de XL
+        while i_count >= 40:
             self.tape.append("XL")
             i_count -= 40
         while i_count >= 10:
             self.tape.append("X")
             i_count -= 10
-        while i_count >= 9:  # Manejo especial de IX
+        while i_count >= 9:
             self.tape.append("IX")
             i_count -= 9
         while i_count >= 5:
             self.tape.append("V")
             i_count -= 5
-        while i_count >= 4:  # Manejo especial de IV
+        while i_count >= 4:
             self.tape.append("IV")
             i_count -= 4
         while i_count >= 1:
             self.tape.append("I")
             i_count -= 1
 
-        print("Resultado simplificado:", ''.join(self.tape))  # Depuración final
         self.current_state = "HALT"  # Termina el programa
 
 # Ejemplo de uso
-input_tape = "CD#XX"  # Representa "400 / 20"
+input_tape = "XV#V"  # Representa "400 / 20"
 turing_machine = TuringMachineRomanDivision(input_tape)
 result = turing_machine.run()
 print("Resultado de la división en números romanos:", result)
